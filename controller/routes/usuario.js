315c9989c0e3af3470944d123a0c1ce02da254f7
Gabrielh7Amorim <gabriel.amorim12@fatec.sp.gov.br>
2022-05-04 20:53:00 -0300

    teste pessoal de cadastro

--- controller/routes/usuario.js
+++ controller/routes/usuario.js
@@ -9,7 +9,21 @@ app.get("/cadastro", function(req, res){
         res.render('../../view/usuario/CadastroUsuario', { mensagem: 'Cadastro' });
     else
         res.render('../../view/usuario/CadastroUsuario', { mensagem: null });
-}) 
+})
+
+// teste pessoal - cadastrar
+app.post('/cadastro/usuario/salvar', function(req, res){
+(async()=>{
+    console.log('INSERT INTO USUARIO');
+    const result = await usuarioBanco.insertUsuario({nome: req.body.nome, senha: req.body.senha});
+    console.log(result);
+})().then(function(){
+    res.send("Post criado com sucesso!")
+}).catch(function(erro){
+    res.send("Houve um erro: " + erro)
+});
+})
+
 
 app.post('/cadastro/usuario/edit/salvar', (req, res) => {
     var usuario = { 
